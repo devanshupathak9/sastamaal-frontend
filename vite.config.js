e1f2aa3aedcb7ba772f01dbf851c7b80bbc823d4
pathakdevanshu9 <pathakdevanshu9@gmail.com>
2026-03-29 02:34:02 +0530

    Updated the vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sastamaal/",
-  server: {
-    allowedHosts: ['valene-downier-melodie.ngrok-free.dev']
-  }
+  base: "/", 
 })
